93814594335e1c1c03cc0860195186c6d914b310
Jari Jokinen <info@jarijokinen.com>
2013-04-02 20:22:19 +0300

    Add installation script for dotfiles

---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+dotfiles=(
+  bash_aliases
+  bash_functions
+  bashrc
+  gemrc
+  gitconfig
+  gitignore
+  muttrc
+  profile
+  railsrc
+  screenrc
+  vim
+  vimrc
+  Xresources
+)
+
+script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+for dotfile in ${dotfiles[@]}; do
+  src="$script_path/$dotfile"
+  dst="$HOME/.$dotfile"
+  cmd="ln -s $src $dst"
+
+  if [ -e $dst ]; then
+    if [ -h $dst ]; then
+      if [ $(readlink $dst) == "$src" ]; then
+        echo "[ skip    ] $dst"
+        continue
+      else
+        echo "[ replace ] $dst"
+        `rm -rf $dst`
+        `$cmd`
+        continue
+      fi
+    else
+      echo "[ backup  ] $dst"
+      backup_cmd="mv $dst $dst.backup"
+      `$backup_cmd`
+      echo "[ replace ] $dst"
+      `$cmd`
+      continue
+    fi
+  else
+    echo "[ create  ] $dst"
+    `$cmd`
+  fi
+done
+
+exit 0
