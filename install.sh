4d650f730a900c2aa44fc99e1d9f24c3881c95d4
Jari Jokinen <info@jarijokinen.com>
2016-07-16 21:26:25 +0300

    Add xfce4 settings

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ dotfiles=(
   bash_functions
   bashrc
   bin
+  config/xfce4/xfconf/xfce-perchannel-xml/keyboards.xml
+  config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
+  config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+  config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+  config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+  config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
   gemrc
   gitconfig
   gitignore
@@ -21,6 +27,8 @@ dotfiles=(
 set -e
 msg() { echo "$1"; }
 script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml
 
 for dotfile in ${dotfiles[@]}; do
   src="$script_path/$dotfile"
