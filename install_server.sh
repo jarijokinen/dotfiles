d76ea5420810d6330c289f2c8702bfaba968c73a
Jari Jokinen <info@jarijokinen.com>
2016-08-17 04:44:17 +0300

    Add install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -4,27 +4,16 @@ dotfiles=(
   bash_functions
   bashrc
   bin
-  config/xfce4/xfconf/xfce-perchannel-xml
   gemrc
-  gitconfig
-  gitignore
-  kernel-pkg.conf
-  muttrc
   profile
-  railsrc
   screenrc
-  themes/axiomd
   vim
   vimrc
-  Xresources
 )
 
 set -e
 msg() { echo "$1"; }
 script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-mkdir -p $HOME/.config/xfce4/xfconf
-mkdir -p $HOME/.themes
 
 for dotfile in ${dotfiles[@]}; do
   src="$script_path/$dotfile"
