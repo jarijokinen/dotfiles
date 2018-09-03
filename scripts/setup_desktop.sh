4d04d8520cd6ba5b537fb0d7f54a000f147d35eb
Jari Jokinen <jari.jokinen@j2.fi>
2018-09-04 01:54:37 +0300

    Add installers for Watchman and Android Studio

--- scripts/setup_desktop.sh
+++ scripts/setup_desktop.sh
@@ -6,6 +6,8 @@
 # This script sets up a desktop environment on base installation of Debian sid.
 
 packages='
+  autoconf
+  automake
   curl
   dbus-x11
   dialog
@@ -20,13 +22,17 @@ packages='
   keepassx
   kernel-package
   less
+  libssl-dev
+  libtool
   lightdm
   lsb-release
+  m4
   man
   nasm
   nautilus-dropbox
   openssh-client
   pavucontrol
+  pkg-config
   policykit-1
   pulseaudio
   python3-pip
@@ -76,6 +82,14 @@ pip_packages='
 set -e
 [[ $EUID -eq 0 ]] || (echo 'E: this script mus be run as root' && exit 1)
 
+show_prompts() {
+  echo 'Creating a regular user account...'
+  read -p "Username: " username
+  read -sp "Password: " user_password && echo
+  adduser $username -q --gecos ',,,' --disabled-password
+  echo "$username:$user_password" | chpasswd
+}
+
 install_packages() {
   echo 'Installing packages...'
   apt-get -qy install $packages
@@ -92,22 +106,54 @@ install_chrome() {
 }
 
 install_nodejs() {
+  echo 'Installing Node.js...'
   curl -sL https://deb.nodesource.com/setup_10.x | bash -
   apt-get -qy install nodejs
 }
 
 install_npm_packages() {
+  echo 'Installing Node packages...'
   npm install -g $npm_packages
 }
 
 install_pip_packages() {
+  echo 'Installing Python packages...'
   pip3 install $pip_packages
 }
 
+install_watchman() {
+  echo 'Installing Watchman...'
+  git clone https://github.com/facebook/watchman.git /tmp/watchman
+  cd /tmp/watchman
+  git checkout v4.9.0
+  ./autogen.sh
+  ./configure --without-pcre --without-python --enable-lenient
+  make
+  make install
+}
+
+install_android_studio() {
+  echo 'Installing Android Studio...'
+  package_url=$(
+    curl -s https://developer.android.com/studio/ |
+    grep -oP https.+?linux.zip | head -1
+  )
+  wget $package_url -O /tmp/android-studio.zip
+  unzip /tmp/android-studio.zip -d /opt
+  usermod -aG plugdev $username
+
+  cat <<-"EOF" >> /etc/udev/rules.d/51-android.rules
+		SUBSYSTEM=="usb", ATTR{idProduct}=="4ee7", MODE="0660", GROUP="plugdev", SYMLINK+="android%n"
+	EOF
+}
+
+show_prompts
 install_packages
 install_chrome
 install_nodejs
 install_npm_packages
 install_pip_packages
+install_watchman
+install_android_studio
 
 exit 0
