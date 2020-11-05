e86394c051bceb6b91478c36ab021697325edffa
Jari Jokinen <jari.jokinen@j2.fi>
2020-11-05 09:32:14 +0200

    Change Downloads directory location

--- install_desktop.sh
+++ install_desktop.sh
@@ -2,35 +2,34 @@
 
 set -e
 
-install_vim_plugins() {
-  echo 'Installing Vim plugins...'
-  f=$HOME/.vim/pack/default/start
-  mkdir -p $f
-  mkdir -p $HOME/.vim/swap
-  git clone https://github.com/vim-airline/vim-airline $f/airline
-  git clone https://github.com/neoclide/coc.nvim $f/coc
-  git clone https://github.com/tomasiser/vim-code-dark $f/code-dark
-  git clone https://github.com/tpope/vim-commentary $f/commentary
-  git clone https://github.com/mattn/emmet-vim $f/emmet
-  git clone https://github.com/sheerun/vim-polyglot $f/polyglot
-  git clone https://github.com/tpope/vim-rails $f/rails
-  git clone https://github.com/tpope/vim-surround $f/surround
-}
+echo 'Installing Vim plugins...'
+f=$HOME/.vim/pack/default/start
+mkdir -p $f
+mkdir -p $HOME/.vim/swap
+git clone https://github.com/vim-airline/vim-airline $f/airline
+git clone https://github.com/neoclide/coc.nvim $f/coc
+git clone https://github.com/tomasiser/vim-code-dark $f/code-dark
+git clone https://github.com/tpope/vim-commentary $f/commentary
+git clone https://github.com/mattn/emmet-vim $f/emmet
+git clone https://github.com/sheerun/vim-polyglot $f/polyglot
+git clone https://github.com/tpope/vim-rails $f/rails
+git clone https://github.com/tpope/vim-surround $f/surround
 
-install_dotfiles() {
-  echo 'Installing dotfiles...'
-	script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-	install -m0600 -b -T $script_path/bashrc $HOME/.bashrc
-	install -m0600 -b -T $script_path/profile $HOME/.profile
-	install -m0600 -b -T $script_path/screenrc $HOME/.screenrc
-	install -m0600 -b -T $script_path/vimrc $HOME/.vimrc
-  cp -pr $script_path/bin $HOME/bin
-	cp -pr $script_path/config $HOME/.config
-	cp -pr $script_path/themes $HOME/.themes
-	cp -pr $script_path/vim/* $HOME/.vim/
-}
+echo 'Installing dotfiles...'
+script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+install -m0600 -b -T $script_path/bashrc $HOME/.bashrc
+install -m0600 -b -T $script_path/profile $HOME/.profile
+install -m0600 -b -T $script_path/screenrc $HOME/.screenrc
+install -m0600 -b -T $script_path/vimrc $HOME/.vimrc
+cp -pr $script_path/bin $HOME/bin
+cp -pr $script_path/config $HOME/.config
+cp -pr $script_path/themes $HOME/.themes
+cp -pr $script_path/vim/* $HOME/.vim/
 
-install_vim_plugins
-install_dotfiles
+echo 'Changing Downloads directory location...'
+mkdir -p $HOME/Dropbox/Downloads
+mv $HOME/Downloads/* $HOME/Dropbox/Downloads/
+rm -rf $HOME/Downloads
+ln -s $HOME/Dropbox/Downloads $HOME/Downloads
 
 exit 0
